93d84d86fd0773fc77621ea0e0ebc7b7281b8efd
ar1naka <117769304+ar1naka@users.noreply.github.com>
2024-09-04 13:44:13 +0300

    Add files via upload

---
+++ Web/hw10/src/components/page/Register.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="form-box">
+    <div class="header">Register</div>
+    <div class="body">
+      <form @submit.prevent="onRegister">
+        <div class="field">
+          <div class="name">
+            <label for="title">Login:</label>
+          </div>
+          <div class="value">
+            <input id="login" name="login" v-model="login"/>
+          </div>
+        </div>
+        <div class="field">
+          <div class="name">
+            <label for="name">Name:</label>
+          </div>
+          <div class="value">
+            <input autofocus id="l" name="name" v-model="name"/>
+          </div>
+        </div>
+        <div class="error">{{ error }}</div>
+        <div class="button-field">
+          <input type="submit" value="Register">
+        </div>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Register",
+  data: function () {
+    return {
+      title: "",
+      text: "",
+      error: ""
+    }
+  },
+  methods: {
+    onRegister: function () {
+      this.error = "";
+      this.$root.$emit("onRegister", this.login, this.name);
+    }
+  },
+  beforeCreate() {
+    this.$root.$on("onRegisterValidationError", (error) => this.error = error);
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
